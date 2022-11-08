6fdd97f05ea55506b52468d2b48ff7ed3adff1b4
Jesper S <js@contechpro.dk>
2022-11-08 08:15:52 +0100

    k

---
+++ SecretMessageMaker/Encrypter.cs
@@ -0,0 +1,33 @@
+﻿namespace SecretMessageMaker
+{
+    public class Encrypter
+    {
+        private string alphabet = "abcdefghijklmnopqrstuvwxyz ";
+
+        public string Encrypt(string message, int shift)
+        {
+            string encryptedMessage = "";
+            foreach (char c in message.ToLower())
+            {
+                int index = alphabet.IndexOf(c);
+                int newIndex = index + shift >= alphabet.Length ? index + shift - alphabet.Length : index + shift;
+                char newChar = alphabet[newIndex];
+                encryptedMessage += newChar;
+            }
+            return encryptedMessage;
+        }
+        
+        public string Decrypt(string message, int shift)
+        {
+            string decryptedMessage = "";
+            foreach (char c in message.ToLower())
+            {
+                int index = alphabet.IndexOf(c);
+                int newIndex = index - shift < 0 ? index - shift + alphabet.Length : index - shift;
+                char newChar = alphabet[newIndex];
+                decryptedMessage += newChar;
+            }
+            return decryptedMessage;
+        }
+    }
+}
---
+++ SecretMessageMaker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SecretMessageMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Encrypter encrypter = new Encrypter();
+            string message = "Hello";
+            string encryptedMessage = encrypter.Encrypt(message, 1);
+            Console.WriteLine(encryptedMessage);
+            string decryptedMessage = encrypter.Decrypt(encryptedMessage, 1);
+            Console.WriteLine(decryptedMessage);
+            Console.Read();
+        }
+    }
+}
